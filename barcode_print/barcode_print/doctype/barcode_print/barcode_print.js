dd0786e8fa3a2ceed4199edfb50a3539c8cb5209
9050nasser <9050nasser@gmail.com>
2023-08-06 00:55:12 +0000

    fix print format and direct print from button

--- barcode_print/barcode_print/doctype/barcode_print/barcode_print.js
+++ barcode_print/barcode_print/doctype/barcode_print/barcode_print.js
@@ -1,11 +1,24 @@
 // Copyright (c) 2023, Mohammed Nasser and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Barcode Print', {
-	refresh: function(frm) {
-		frm.add_custom_button(__('Print Barcode'), function(){
-			
-			cur_frm.print_doc();			
-		})
-	}
+frappe.ui.form.on("Barcode Print", { 
+	refresh: function(frm, cdt, cdn) {
+	frm.add_custom_button(__('Print Barcode'), function(){
+	var me = this;
+	var doc = frm.doc
+	var print_format = "Barcode"; // print format name
+	
+	var w = window.open(frappe.urllib.get_full_url("/printview?"
+		+"doctype="+encodeURIComponent(cdt)
+		+"&name="+encodeURIComponent(cdn)
+		+"&trigger_print=1"
+		+"&format=" + print_format
+		+"&no_letterhead=0"
+		+("ar" ? ("&_lang="+"ar") : "")));
+	
+		if(!w) {
+			msgprint(__("Please enable pop-ups for printing.")); return;
+		}
+})}
 });
+	
